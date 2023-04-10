8082ea3890dc361ed97f34bc13eb936790cb2aca
Saullo Reis <saulloreis01@hotmail.com>
2023-04-10 10:14:05 -0300

    criando o home

---
+++ src/intro/intro.tsx
@@ -0,0 +1,18 @@
+import './style.sass'
+
+export const Intro = () => {
+    return (
+      <section className="intro">
+        <h1 className="intro-name">Olá, eu sou Saullo Reis</h1>
+        <h1 className="intro-work">Desenvolvedor Web Júnior</h1>
+        <p className="intro-about">
+          Iniciando na programação busco evoluir e aprender todos os dias,
+          atráves de cursos, pessoas e alguns vídeos pela internet. Tenho
+          facilidade em aprender e conseguir resolver problemas.
+        </p>
+        <a  className='intro-curriculo' href="./assets/curriculo" download>
+          Currículo
+        </a>
+      </section>
+    );
+}
