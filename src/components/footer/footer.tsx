e900077a5819c74ab8d20b80c248a64f02eb7968
Saullo Reis <saulloreis01@hotmail.com>
2023-04-12 10:39:52 -0300

    criando footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,10 @@
+import "./style.sass"
+
+export const Footer = () => {
+    return (
+      <section className="footer">
+        <img className="footer-img" src="https://i.postimg.cc/wTMJmWnn/icon.png"></img>
+        <p className="footer-text">Saullo Reis &copy; 2023</p>
+      </section>
+    );
+}
